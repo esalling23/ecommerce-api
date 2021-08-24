ecb2ed7e5f46cab478059d0fb74ff2ff6d53cd19
esalling23 <esalling23@gmail.com>
2021-08-24 14:07:07 -0400

    Refactors order routes
    Adds findOrderMiddleware to attach order object to request for
    show/update/delete
    Adds `router.use` to simplify requireToken middleware usage

--- app/routes/order_routes.js
+++ app/routes/order_routes.js
@@ -11,6 +11,15 @@ const {
 
 const requireToken = require('../../lib/require_token')
 
+const findOrderMiddleware = (req, res, next) => {
+  Order.findById(req.params.id)
+    .then(handle404)
+    .then(order => requireOwnership(req, order))
+    .then(order => req.order = order)
+    .then(() => next())
+    .catch(next)
+}
+
 const indexOrders = (req, res, next) => {
   Order.find({ owner: req.user.id })
     .then(orders => res.json({ orders }))
@@ -18,11 +27,7 @@ const indexOrders = (req, res, next) => {
 }
 
 const showOrder = (req, res, next) => {
-  Order.findById(req.params.id)
-    .then(handle404)
-    .then(order => requireOwnership(req, order))
-    .then(order => res.json({ order }))
-    .catch(next)
+  res.json({ order: req.order })
 }
 
 const createOrder = (req, res, next) => {
@@ -36,33 +41,34 @@ const updateOrder = (req, res, next) => {
   if (!req.body.productId) {
     next(new BadParamsError('Missing Product ID for order update'))
   }
-  Order.findById(req.params.id)
-    .then(handle404)
-    .then(order => requireOwnership(req, order))
-    .then(order => {
-      order.products.push(req.body.productId)
-      return order.save()
-    })
+
+  req.order.products.push(req.body.productId)
+
+  req.order.save()
     .then(order => res.json({ order }))
     .catch(next)
 }
 
 const deleteOrder = (req, res, next) => {
-  Order.findById(req.params.id)
-    .then(handle404)
-    .then(order => requireOwnership(req, order))
-    .then(order => order.deleteOne())
+  req.order.deleteOne()
     .then(() => res.sendStatus(204))
     .catch(next)
 }
 
+// Middleware
+router.use(requireToken)
+
 // Create & Index routes
 router.route('/orders')
-  .post(requireToken, createOrder)
-  .get(requireToken, indexOrders)
+  .post(createOrder)
+  .get(indexOrders)
 
 // Update, Delete, & Show routes
 router.route('/orders/:id')
+  .patch(findOrderMiddleware, updateOrder)
+  .get(findOrderMiddleware, showOrder)
+  .delete(findOrderMiddleware, deleteOrder)
+
   .patch(requireToken, updateOrder)
   .get(requireToken, showOrder)
   .delete(requireToken, deleteOrder)
