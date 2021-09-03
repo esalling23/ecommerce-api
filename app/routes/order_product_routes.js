f01262cb4356078ff701db2aa72ebdd74e5eeca4
esalling23 <esalling23@gmail.com>
2021-09-03 13:30:22 -0400

    Adds router for routes to update products in an order
    Adds POST to add product to order - if product exists already,
    will add to the product count
    Adds PATCH to update product on order -
    requires product to already be on order, replaces count
    Adds DELETE to remvoe product on order -
    requires product to already be on order

---
+++ app/routes/order_product_routes.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+
+const { calculateTotalPrice } = require('../../lib/price_helpers')
+const {
+  requireProductId,
+  requireProductInOrder,
+  requireOpenOrder,
+  BadParamsError
+} = require('../../lib/custom_errors')
+const { findProductInOrder, findProduct } = require('../../lib/find_documents')
+
+const Order = require('../models/order')
+
+// POST /orders/:orderId/update
+// Adds new product to order. If product already exists in order, updates count.
+const addToOrder = (req, res, next) => {
+  console.log(req.product, req.order)
+  Promise.resolve()
+    .then(() => {
+      if (!req.body.count) {
+        req.body.count = 1
+      }
+    })
+    .then(() => {
+      if (req.productInOrder.product) {
+        const { product, index } = req.productInOrder
+        product.count += req.body.count
+        req.order.products.splice(index, 1, product)
+      } else {
+        req.order.products.push({
+          productRef: req.body.productId,
+          count: req.body.count
+        })
+      }
+      return req.order.save()
+    })
+    .then(order => Order.findById(order.id).populate('products.productRef'))
+    .then(order => {
+      order.totalPrice = calculateTotalPrice(order.products).centsTotal
+      return order.save()
+    })
+    .then(order => res.json({ order: order.toObject() }))
+    .catch(next)
+}
+
+// PATCH /orders/:orderId/update/:productId
+// Updates product on order (count)
+const updateOrder = (req, res, next) => {
+  Promise.resolve()
+    .then(() => (req.order))
+    .then(() => {
+      if (!req.body.count) {
+        throw new BadParamsError('Provide a count value greater than 1')
+      }
+    })
+    .then(() => {
+      const { product, index } = req.productInOrder
+      product.count = req.body.count
+      req.order.products.splice(index, 1, product)
+      return req.order.save()
+    })
+    .then(order => {
+      // // populate the productRef on each cart-product object
+      //
+      res.json({ order })
+    })
+    .catch(next)
+}
+
+// DELETE /orders/:orderId/delete/:productId
+// Removes product from order
+const removeFromOrder = (req, res, next) => {
+  const prodIndex = req.order.products.indexOf(req.product)
+
+  req.order.products.splice(prodIndex, 1)
+  req.order.save()
+    .then(order => res.status(203).json({ order }))
+    .catch(next)
+}
+
+router.use(requireProductId, requireOpenOrder, findProduct, findProductInOrder)
+
+router.route('/products')
+  .patch(requireProductInOrder, updateOrder)
+  .post(addToOrder)
+  .delete(requireProductInOrder, removeFromOrder)
+
+module.exports = router
--- app/routes/order_routes.js
+++ app/routes/order_routes.js
@@ -2,26 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const Order = require('../models/order')
-const Product = require('../models/product')
 
-const {
-  handle404,
-  handleOrderCompleted,
-  requireOwnership,
-  BadParamsError
-} = require('../../lib/custom_errors')
+const { requireOpenOrder } = require('../../lib/custom_errors')
 const { calculateTotalPrice } = require('../../lib/price_helpers')
+const { findOrder } = require('../../lib/find_documents')
 const requireToken = require('../../lib/require_token')
 
-const findOrderMiddleware = (req, res, next) => {
-  Order.findById(req.params.id)
-    .populate('products.productRef')
-    .then(handle404)
-    .then(order => requireOwnership(req, order))
-    .then(order => { req.order = order })
-    .then(() => next())
-    .catch(next)
-}
+const orderProductsRouter = require('./order_product_routes')
 
 // Query param `completed` allowed for filtering
 // ?completed=true to get only completed orders
@@ -57,70 +44,9 @@ const createOrder = (req, res, next) => {
     .catch(next)
 }
 
-// PATCH /orders/:id/add
-// Accepts { productId: 'id', count: 3 } as body data
-// Will add product data to the order - if product exists, increases count by provided data
-// if passed the query param `updateProduct` (set to `true`), will replace product count with provided data
-// if passed the query param `deleteProduct` (set to `true`), will delete the product from the cart
-const updateOrder = (req, res, next) => {
-  const { updateProduct, deleteProduct } = req.query
-
-  Promise.resolve()
-    .then(() => {
-      if (!req.body.productId) {
-        throw new BadParamsError('Missing Product ID for order update')
-      }
-      if (!req.body.count && updateProduct) {
-        throw new BadParamsError('Provide a count value greater than 1')
-      } else if (!req.body.count) {
-        req.body.count = 1
-      }
-    })
-    .then(() => handleOrderCompleted(req.order))
-    .then(() => Product.findById(req.body.productId))
-    .then(product => handle404(product, 'Product ID Invalid'))
-    .then(product => {
-      // check if this product exists already, we can just add another one
-      const currProd = req.order.products.find(el => el.productRef.equals(product.id))
-      if (currProd) {
-        const prodIndex = req.order.products.indexOf(currProd)
-        if (deleteProduct) {
-          req.order.products.splice(prodIndex, 1)
-        } else {
-          if (updateProduct) {
-            currProd.count = req.body.count
-          } else {
-            currProd.count += req.body.count
-          }
-          req.order.products.splice(prodIndex, 1, currProd)
-        }
-      } else if (updateProduct || deleteProduct) {
-        // If updating & no currProd, throw error
-        throw new BadParamsError(`Cannot ${updateProduct ? 'update' : 'remove'} product not in cart`)
-      } else {
-        req.order.products.push({
-          productRef: product.id,
-          count: req.body.count
-        })
-      }
-      return req.order.save()
-    })
-    .then(order => {
-      // populate the productRef on each cart-product object
-      return Order.findById(order.id).populate('products.productRef')
-    })
-    .then(order => {
-      order.totalPrice = calculateTotalPrice(order.products).centsTotal
-      return order.save()
-    })
-    .then(order => order.toObject())
-    .then(order => res.json({ order }))
-    .catch(next)
-}
 
 const checkoutOrder = (req, res, next) => {
   Promise.resolve()
-    .then(() => handleOrderCompleted(req.order))
     .then(() => {
       req.order.completed = true
       req.order.totalPrice = calculateTotalPrice(req.order.products).centsTotal
@@ -145,12 +71,14 @@ router.route('/')
   .get(indexOrders)
 
 // Update, Delete, & Show routes
-router.route('/:id')
-  .patch(findOrderMiddleware, updateOrder)
-  .get(findOrderMiddleware, showOrder)
-  .delete(findOrderMiddleware, deleteOrder)
+router.route('/:orderId')
+  .get(findOrder, showOrder)
+  .delete(findOrder, deleteOrder)
+
+// Include orderProductsRouter at `/:id` endpoint
+router.use('/:orderId', findOrder, orderProductsRouter)
 
 // Custom Routes
-router.patch('/:id/checkout', findOrderMiddleware, checkoutOrder)
+router.patch('/:orderId/checkout', requireOpenOrder, findOrder, checkoutOrder)
 
 module.exports = router
