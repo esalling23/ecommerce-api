020a968c5f07c16fefb2165592162c3d9de71c00
esalling23 <esalling23@gmail.com>
2021-09-05 16:36:35 -0400

    Updates stripe endpoint to work with new middleware

--- app/routes/stripe_routes.js
+++ app/routes/stripe_routes.js
@@ -4,28 +4,32 @@ const router = express.Router()
 
 const Order = require('../models/order')
 
-const { handleOrderCompleted } = require('../../lib/custom_errors')
+const { requireOpenOrder } = require('../../lib/custom_errors')
 const requireToken = require('../../lib/require_token')
+const { findOrder } = require('../../lib/find_documents')
 const { calculateTotalPrice } = require('../../lib/price_helpers')
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-router.post('/payment-intent/:orderId', requireToken, async (req, res, next) => {
-  try {
-    const order = await Order.findById(req.params.orderId).populate('products.productRef')
-    handleOrderCompleted(order)
-    const { centsTotal, displayTotal } = calculateTotalPrice(order.products)
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: centsTotal,
-      currency: 'usd'
-    })
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-      totalPrice: displayTotal
-    })
-  } catch (err) {
-    next(err)
-  }
-})
+router.post('/payment-intent/:orderId',
+  requireToken,
+  findOrder,
+  requireOpenOrder,
+  async (req, res, next) => {
+    try {
+      const order = await Order.findById(req.params.orderId).populate('products.productRef')
+      const { centsTotal, displayTotal } = calculateTotalPrice(order.products)
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: centsTotal,
+        currency: 'usd'
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+        totalPrice: displayTotal
+      })
+    } catch (err) {
+      next(err)
+    }
+  })
 
 module.exports = router
